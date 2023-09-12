1cb51cda34434f5f75f0167eaa9d75063af0be46
andreslopezma <andres.lopezma@hotmail.com>
2023-09-11 20:50:12 -0500

    Se agrega el cors necesario

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const sequelize = require('./database');
 const movieRoutes = require('./routes/movieRoutes');
 const characterRoutes = require('./routes/characterRoutes');
 
+const cors = require('cors');
+
 const app = express();
 const PORT = 3000;
 
 // Midlleware para parsear JSON
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 // routes
 app.use(movieRoutes);
