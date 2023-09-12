ff137011001b0581e17fbdefafdcf2540b01e20e
andreslopezma <andres.lopezma@hotmail.com>
2023-09-11 20:54:08 -0500

    Se agrega una nueva ruta para consultar solamente 1 pelicula

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const movieController = require('../controllers/movieController');
 
 router.get('/movies', movieController.getAllMovies);
+router.get('/movie/:id', movieController.getAllMovie);
 router.post('/movies', movieController.createMovie);
 router.put('/movie/:id', movieController.updateMovie);
 router.delete('/movie/:id', movieController.deleteMovie);
