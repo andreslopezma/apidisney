335483ab51d08e2a8e5fee3884d1428c2f419fed
andreslopezma <andres.lopezma@hotmail.com>
2023-09-13 17:25:53 -0500

    Se agrega el campo image

--- migrations/20230910224354-create-movies.js
+++ migrations/20230910224354-create-movies.js
@@ -29,6 +29,9 @@ module.exports = {
         type: Sequelize.BOOLEAN,
         defaultValue: false
       },
+      image: {
+        type: Sequelize.STRING
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
